9a494d4ee2524e560a2fe504fc81c5fdc741d329
nikolai <ollikahnstex@rambler.ru>
2022-04-12 16:45:56 +0300

    initial commit

---
+++ test.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+if __name__ == '__main__':
+    x = np.array([[1, 2, 3]])
+    y = np.array([[1], [2], [3]])
+    print(x+y)
+    print(x*y)
+    print(x//y)
+    print("master_fixed")
+    print("dev")
+    print("dev_old")
